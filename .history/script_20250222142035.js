1d573ebb19f3ec7d789ab7ff23c2d728363cc147
yago <goncalveslima2005@gmail.com>
2025-02-22 14:20:42 -0300

    js

---
+++ .history/script_20250222142035.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menuMobile = document.querySelector('.menu-mobile');
+    const menuDesktop = document.querySelector('.menu-desktop');
+    menuMobile.classList.toggle('ativo');
+    menuDesktop.classList.toggle('ativo');
+}
