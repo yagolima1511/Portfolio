72342d2f995f1a589aac1a8ff8b64cffd235e5ae
yago <goncalveslima2005@gmail.com>
2025-02-22 14:23:30 -0300

    delete js

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    const menuMobile = document.querySelector('.menu-mobile');
-    const menuDesktop = document.querySelector('.menu-desktop');
-    menuMobile.classList.toggle('ativo');
-    menuDesktop.classList.toggle('ativo');
-}
